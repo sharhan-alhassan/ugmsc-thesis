b6b29847bb2af888b3f51f676ad3cf8ec6edb2f4
sharhan-alhassan <sharhanalhassan@gmail.com>
2023-10-08 18:22:35 +0000

    patch: updated

--- server_setup.sh
+++ server_setup.sh
@@ -6,3 +6,4 @@ sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3.10-venv -y
 sudo apt install python3-pip -y
+git clone https://github.com/sharhan-alhassan/ugmsc-thesis.git && cd upgmsc-thesis
