3f09f84fee9d4c19441c88061b897e46876799ea
sharhan-alhassan <sharhanalhassan@gmail.com>
2023-10-08 17:22:46 +0000

    patch: updated

---
+++ pyrapl/optimized_2_algorithms.py
@@ -0,0 +1,63 @@
+import pyRAPL
+import time
+import json
+import hashlib
+
+def calculate_merkle_root(data, hash_function):
+    if len(data) == 1:
+        return data[0]
+
+    if len(data) % 2 != 0:
+        data.append(data[-1])
+
+    new_level = []
+    for i in range(0, len(data), 2):
+        combined = data[i] + data[i+1]
+        new_hash = hash_function(combined).digest()
+        new_level.append(new_hash)
+    
+    return calculate_merkle_root(new_level, hash_function)
+
+
+def main():
+    # Read data from the "transactions.json" file
+    with open("./data/50000.json", "r") as json_file:
+        transactions = json.load(json_file)
+
+    # Convert each transaction dictionary to bytes and hash them
+    byte_numbers = [json.dumps(transaction, sort_keys=True).encode() for transaction in transactions]
+
+    # Define the hash functions for each group
+    hash_functions = [
+        hashlib.sha3_384,
+        hashlib.sha3_224,
+        # hashlib.sha384,
+        # hashlib.sha224
+    ]
+
+    # Initial hash of individual members of the list
+    hash_list = [hash_function(number).digest() for number, hash_function in zip(byte_numbers, hash_functions)]
+
+    # Grouping in pairs and hashing
+    while len(hash_list) > 1:
+        new_level = []
+        for i in range(0, len(hash_list), 2):
+            combined = hash_list[i] + hash_list[i+1]
+            new_hash = hash_functions[1](combined).digest()
+            new_level.append(new_hash)
+        hash_list = new_level
+
+    # Final Merkle root
+    final_merkle_root = hash_list[0]
+    print("Final Merkle Root:", final_merkle_root.hex())
+
+pyRAPL.setup()
+measure = pyRAPL.Measurement('bar')
+with measure:
+    measure.begin()
+
+    main()
+
+    measure.end()
+    
+
---
+++ pyrapl/optimized_all_3_algorithms copy.py
@@ -0,0 +1,59 @@
+import pyRAPL
+import time
+import json
+import hashlib
+
+def calculate_merkle_root(data, hash_functions):
+    if len(data) == 1:
+        return data[0]
+
+    if len(data) % 2 != 0:
+        data.append(data[-1])
+
+    new_level = []
+    num_steps = len(hash_functions)
+    hash_function_index = 0
+    
+    for i in range(0, len(data), 2):
+        combined = data[i] + data[i + 1]
+        new_hash = hash_functions[hash_function_index](combined).digest()
+        new_level.append(new_hash)
+        
+        num_steps -= 1
+        if num_steps == 0:
+            num_steps = len(hash_functions)
+            hash_function_index = (hash_function_index + 1) % len(hash_functions)
+    
+    return calculate_merkle_root(new_level, hash_functions)
+
+
+def main():
+    # Read data from the "transactions.json" file
+    with open("./code/data/50000.json", "r") as json_file:
+        transactions = json.load(json_file)
+
+    # Convert each transaction dictionary to bytes and hash them
+    byte_numbers = [json.dumps(transaction, sort_keys=True).encode() for transaction in transactions]
+
+    # Define the hash functions for each group
+    hash_functions = [
+        hashlib.sha3_384,
+        hashlib.sha3_224,
+        hashlib.sha384,
+        # hashlib.sha224, 0596538336
+    ]
+
+    # Calculate the Merkle root using the available hash functions
+    final_merkle_root = calculate_merkle_root(byte_numbers, hash_functions)
+    print("Final Merkle Root:", final_merkle_root.hex())
+
+pyRAPL.setup()
+measure = pyRAPL.Measurement('bar')
+with measure:
+    measure.begin()
+
+    main()
+
+    measure.end()
+    
+
