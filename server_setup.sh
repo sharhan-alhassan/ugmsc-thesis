10681905062831861998ead575b910a193bd7072
sharhan-alhassan <sharhanalhassan@gmail.com>
2023-10-08 19:09:10 +0000

    patch: updated

--- server_setup.sh
+++ server_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update -y
 sudo apt install python3 -y
